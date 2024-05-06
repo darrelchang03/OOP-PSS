4965822e4a02fceed960d8dfd46d18d4795dfe60
Cody <codyapolinar@yahoo.com>
2024-05-05 23:55:46 -0700

    added new methods for viewer

--- Viewer.py
+++ Viewer.py
@@ -20,6 +20,33 @@ class Viewer():
         print(message)
     
     # Other public Methods: Display day, display week, display month
+        
+
+    month = 1
+    week = 1
+    day = 1
+    #Starts calendar on first day of first week of month
+
+    #need to figure out parameters later but for now there's presumed parameters on display methods
+    def display_month(month):
+        print("Month: " + month)
+        
+    def display_week(week):
+        print("Week: " + week)
+
+    def display_day(day):
+        print("Day: " + day)
+    
 
     # Private Methods
+        #should be something that updates month day and week depending on the controller
+
+    def update_month(newMonth):
+        month = newMonth
+        
+    def update_week(newWeek):
+        week = newWeek
+
+    def update_day(newDay):
+        day = newDay
         
